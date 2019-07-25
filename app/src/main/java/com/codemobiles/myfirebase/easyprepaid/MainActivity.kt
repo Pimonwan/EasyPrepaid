df2fab7f9360209499da6ea861c9d8bdc9625e1d
pattaragun.chimphet <pattaragun.chimphet@scb.co.th>
2019-07-25 15:16:36 +0700

    Main and Scan

--- app/src/main/java/com/codemobiles/myfirebase/easyprepaid/MainActivity.kt
+++ app/src/main/java/com/codemobiles/myfirebase/easyprepaid/MainActivity.kt
@@ -2,19 +2,30 @@ package com.codemobiles.myfirebase.easyprepaid
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var btn: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tvresult = findViewById(R.id.tvresult) as TextView
 
-        // button
-        topage2.setOnClickListener{
-            startActivity(Intent(applicationContext, Menu::class.java))
+        btn = findViewById(R.id.ScanQR) as ImageView
+
+        btn!!.setOnClickListener {
+            val intent = Intent(this@MainActivity, ScanActivity::class.java)
+            startActivity(intent)
         }
+
+    }
+
+    companion object {
+
+        var tvresult: TextView? = null
     }
 }
